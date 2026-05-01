d1824991891b8da5130e6a34fb6f1423d5791645
evgeniy <akusev1107@gmail.com>
2026-05-01 15:29:22 +0300

    Релиз 0.7.1 - Забыл про шаблон документа Дргое

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.whitewolf.documents"
         minSdk = 28
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.7"
+        versionCode = 11
+        versionName = "0.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
